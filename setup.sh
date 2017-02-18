cb52f49be5ed3930fcaef385ac6d21dcac56ffa9
kevinkiklee <kevin.kik.lee@gmail.com>
2017-02-18 09:31:38 -0500

    Add Heroku remote to setup script

--- setup.sh
+++ setup.sh
@@ -3,3 +3,4 @@ bundle install
 bundle exec rake db:setup
 git config --local user.name iserbit
 git config --local user.email kevin.kik.lee@gmail.com
+git remote add heroku https://git.heroku.com/slackoff.git
