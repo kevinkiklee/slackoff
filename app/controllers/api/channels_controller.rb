4f8811779dff977dd4b456a9976f55d8000ac503
kevinkiklee <kevin.kik.lee@gmail.com>
2017-02-18 10:10:29 -0500

    Fix channel API message ordering bug

--- app/controllers/api/channels_controller.rb
+++ app/controllers/api/channels_controller.rb
@@ -1,7 +1,8 @@
 class Api::ChannelsController < ApplicationController
   def show
     @channel = Channel.find(params[:id])
-    @messages = @channel.messages.reverse
+    @messages = @channel.messages.order(:created_at).reverse
+    # @messages = @channel.messages.reverse
     render 'api/channels/show'
   end
 end
