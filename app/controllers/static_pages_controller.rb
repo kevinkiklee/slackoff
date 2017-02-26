a37a5e728fa63cc52bb821da63d7c513fa1addea
kevinkiklee <kevin.kik.lee@gmail.com>
2017-02-26 13:34:04 -0500

    Implement OG Meta-tags

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,5 +4,14 @@ class StaticPagesController < ApplicationController
       @channels = current_user.channels.where(private: false).order(:name)
       @direct_messages = current_user.channels.includes(:users).where(private: true).order(:name)
     end
+
+    og_image_url = root_url + ActionController::Base.helpers.asset_url("slackoff-og.jpg")
+
+    set_meta_tags og: {
+      title:    'SlackOff',
+      url:      'http://slackoff.com',
+      image:    og_image_url,
+      description: 'SlackOff is a full-stack web application inspired by Slack. It utilizes Ruby on Rails, React.js with Redux pattern, and Pusher API for websocket protocol.'
+    }
   end
 end
