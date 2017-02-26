ef43b4614eb088febe35223b2fe43651ce9b2d31
kevinkiklee <kevin.kik.lee@gmail.com>
2017-02-26 16:14:30 -0500

    Prepare for avatar functionality

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,10 +1,6 @@
 # config/initializers/pusher.rb
 require 'pusher'
 
-# Pusher.app_id = '301567'
-# Pusher.key = 'd46870f8b7c4c1636fca'
-# Pusher.secret = 'a700f068ed42ab7bd614'
-
 Pusher.app_id = ENV["PUSHER_APP_ID"]
 Pusher.key    = ENV["PUSHER_KEY"]
 Pusher.secret = ENV["PUSHER_SECRET"]
