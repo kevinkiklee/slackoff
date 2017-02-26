6910a1ba2bac3d5e0e450d0473d04c3eaa4726f5
kevinkiklee <kevin.kik.lee@gmail.com>
2017-02-26 15:03:40 -0500

    Implement Figaro

--- config/initializers/pusher.rb
+++ config/initializers/pusher.rb
@@ -1,13 +1,13 @@
 # config/initializers/pusher.rb
 require 'pusher'
 
-Pusher.app_id = '301567'
-Pusher.key = 'd46870f8b7c4c1636fca'
-Pusher.secret = 'a700f068ed42ab7bd614'
+# Pusher.app_id = '301567'
+# Pusher.key = 'd46870f8b7c4c1636fca'
+# Pusher.secret = 'a700f068ed42ab7bd614'
 
-# Pusher.app_id = ENV["PUSHER_APP_ID"]
-# Pusher.key    = ENV["PUSHER_KEY"]
-# Pusher.secret = ENV["PUSHER_SECRET"]
+Pusher.app_id = ENV["PUSHER_APP_ID"]
+Pusher.key    = ENV["PUSHER_KEY"]
+Pusher.secret = ENV["PUSHER_SECRET"]
 
 # Pusher.app_id = ENV['pusher_app_id']
 # Pusher.key = ENV['pusher_key']
