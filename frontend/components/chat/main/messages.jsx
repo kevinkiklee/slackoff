698241ce655b62dcdabdd45dc48fa609e7b2e17b
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-05 17:07:16 -0400

    Remove unnecessary Pusher connection when changing channels

--- frontend/components/chat/main/messages.jsx
+++ frontend/components/chat/main/messages.jsx
@@ -51,12 +51,12 @@ class Messages extends React.Component {
     const channelLoaded = this.props.channel.name !== undefined;
 
     if(channelLoaded && this.props.channel.name !== newProps.channel.name){
-      // this.pusher.unsubscribe(this.props.channel.id.toString());
-      this.pusher.disconnect();
-
-      this.pusher = new Pusher('d46870f8b7c4c1636fca', {
-        encrypted: true
-      });
+      this.pusher.unsubscribe(this.props.channel.id.toString());
+      // this.pusher.disconnect();
+      //
+      // this.pusher = new Pusher('d46870f8b7c4c1636fca', {
+      //   encrypted: true
+      // });
 
       this.channel = this.pusher.subscribe(newProps.channel.id.toString());
 
