25e0107604225cfe43f41e5078e8acb4c9b56499
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-11 21:15:47 -0400

    Re-add webpack config file

---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  context: __dirname,
+  entry: './frontend/slackoff.jsx',
+  output: {
+    path: path.resolve(__dirname, 'app', 'assets', 'javascripts'),
+    filename: 'bundle.js'
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
