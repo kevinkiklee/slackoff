16826a92ca450c70eeca26312cd372c3c5f5dbc8
kevinkiklee <kevin.kik.lee@gmail.com>
2017-04-30 16:44:43 -0400

    Load redux-logger only in dev environment

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -6,13 +6,17 @@ import createLogger from 'redux-logger';
 import merge from 'lodash/merge';
 
 const logger = createLogger();
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
 
 const configureStore = (preloadedState = {}) => {
   return createStore (
     rootReducer,
     preloadedState,
-    // applyMiddleware(thunk)
-    applyMiddleware(thunk, logger)
+    applyMiddleware(...middlewares)
   );
 };
 
