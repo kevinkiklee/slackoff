a9fb831be358d7e9cbf7e16c71718c7f26873658
kevinkiklee <kevin.kik.lee@gmail.com>
2017-10-02 15:03:43 -0400

    Lint the entry file

--- frontend/slackoff.jsx
+++ frontend/slackoff.jsx
@@ -1,29 +1,30 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Root from './components/root';
-import configureStore from './store/store';
+/* eslint-disable no-console */
+import React from 'react'
+import ReactDOM from 'react-dom'
+import Modal from 'react-modal'
 
-import Modal from 'react-modal';
-
-import merge from 'lodash/merge';
+import Root from './components/root'
+import configureStore from './store/store'
 
 document.addEventListener('DOMContentLoaded', () => {
-  console.log('%cThank you for visiting SlackOff!  Please e-mail me at kevin.kik.lee@gmail.com for more information~', 'background: #ffffff; color: #0000ff');
+  console.log('%cThank you for visiting SlackOff!  Please e-mail me at kevin.kik.lee@gmail.com for more information~', 'background: #ffffff color: #0000ff')
 
-  const root = document.getElementById('root');
-  Modal.setAppElement(document.body);
+  const root = document.getElementById('root')
+  Modal.setAppElement(document.body)
 
-  let store = configureStore();
+  let store = configureStore()
 
   if (window.currentUser) {
     const preloadedState = {
-      session: { currentUser: window.currentUser }
-    };
+      session: {
+        currentUser: window.currentUser,
+      },
+    }
 
-    store = configureStore(preloadedState);
+    store = configureStore(preloadedState)
   }
 
-  window.store = store;
+  window.store = store
 
-  ReactDOM.render(<Root store={ store }/>, root);
-});
+  ReactDOM.render(<Root store={store} />, root)
+})
